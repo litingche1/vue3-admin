432776e1e02417f8d3fbc9605e286573b2b6a2d1
李廷琛 <2496272528@qq.com>
2021-05-07 23:59:27 +0800

    完成验证码插件

--- src/views/acount/login.vue
+++ src/views/acount/login.vue
@@ -3,11 +3,11 @@
     <div class="form-wrap">
       <a-form name="custom-validation" ref="ruleForm">
         <a-form-item>
-          <label for="">用户名</label>
+          <label for>用户名</label>
           <a-input type="password" autocomplete="off" />
         </a-form-item>
         <a-form-item class="password">
-          <label for="">密码</label>
+          <label for>密码</label>
           <a-input type="password" autocomplete="off" />
         </a-form-item>
         <!--                <a-form-item name="age">-->
@@ -18,14 +18,12 @@
           <!--No-Captcha渲染的位置，其中 class 中必须包含 nc-container-->
         </a-form-item>
         <a-form-item>
-          <a-button type="primary" html-type="submit" block>
-            登录
-          </a-button>
+          <a-button type="primary" html-type="submit" block>登录</a-button>
         </a-form-item>
       </a-form>
       <div class="text-center fs-12">
-        <a href="" class="color-white">忘记密码</a> |
-        <a href="" class="color-white">注册</a>
+        <a href class="color-white">忘记密码</a> |
+        <a href class="color-white">注册</a>
       </div>
     </div>
   </div>
@@ -44,7 +42,51 @@ export default {
         wrapperCol: { span: 14 }
       }
     });
-    onMounted(() => {});
+    onMounted(() => {
+      var nc_token = [
+        "FFFF0N00000000009931",
+        new Date().getTime(),
+        Math.random()
+      ].join(":"); // 没有做任何的响应
+      var NC_Opt = {
+        renderTo: "#captcha-dom",
+        appkey: "FFFF0N00000000009931",
+        scene: "nc_login",
+        token: nc_token,
+        customWidth: 300,
+        trans: { key1: "code0" },
+        elementID: ["usernameID"],
+        is_Opt: 0,
+        language: "cn",
+        isEnabled: true,
+        timeout: 3000,
+        times: 5,
+        apimap: {
+          // 'analyze': '//a.com/nocaptcha/analyze.jsonp',
+          // 'get_captcha': '//b.com/get_captcha/ver3',
+          // 'get_captcha': '//pin3.aliyun.com/get_captcha/ver3'
+          // 'get_img': '//c.com/get_img',
+          // 'checkcode': '//d.com/captcha/checkcode.jsonp',
+          // 'umid_Url': '//e.com/security/umscript/3.2.1/um.js',
+          // 'uab_Url': '//aeu.alicdn.com/js/uac/909.js',
+          // 'umid_serUrl': 'https://g.com/service/um.json'
+        },
+        callback: function(data) {
+          window.console && console.log(nc_token);
+          window.console && console.log(data.csessionid);
+          window.console && console.log(data.sig);
+        }
+      };
+      var nc = new noCaptcha(NC_Opt);
+      nc.upLang("cn", {
+        _startTEXT: "请按住滑块，拖动到最右边",
+        _yesTEXT: "验证通过",
+        _error300:
+          '哎呀，出错了，点击<a href="javascript:__nc.reset()">刷新</a>再来一次',
+        _errorNetwork:
+          '网络不给力，请<a href="javascript:__nc.reset()">点击刷新</a>'
+      });
+    });
     return {
       fromConfig
     };
@@ -90,9 +132,9 @@ export default {
     .nc-container #nc_1_wrapper {
       width: 100%;
     }
-    .nc-container {
-      height: 34px;
-    }
+    // .nc-container {
+    //   height: 34px;
+    // }
     .nc-container .nc_scale span {
       height: 34px;
     }
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,20 @@
 // vue.config.js
 module.exports = {
-    // 选项...
-    publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
-    lintOnSave: process.env.NODE_ENV !== 'production',
-    css: {
-        loaderOptions: {
-          // 给 sass-loader 传递选项
-          sass: {
-            // @/ 是 src/ 的别名
-            // 所以这里假设你有 `src/variables.sass` 这个文件
-            // 注意：在 sass-loader v8 中，这个选项名是 "prependData"
-            prependData: `@import "./src/style/main.scss";`
-          },
-        }
-      }
- 
+  // 选项...
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  // eslint-disable-next-line no-dupe-keys
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      // 给 sass-loader 传递选项
+      sass: {
+        // @/ 是 src/ 的别名
+        // 所以这里假设你有 `src/variables.sass` 这个文件
+        // 注意：在 sass-loader v8 中，这个选项名是 "prependData"
+        prependData: `@import "./src/style/main.scss";`
+      },
+    }
+  }
+
 }

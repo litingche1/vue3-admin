0099e9bbbf08436ee564b3e3f749b6f1870af914
litingchen <2496272528@qq.com>
2021-05-07 18:10:58 +0800

    登录页面发开发

--- src/views/acount/login.vue
+++ src/views/acount/login.vue
@@ -1,36 +1,101 @@
 <template>
-    <div>
-        登录
-        <a-button type="primary">
-            Primary
-        </a-button>
-        <a-button>Default</a-button>
-        <a-button type="dashed">
-            Dashed
-        </a-button>
-        <a-button type="danger">
-            Danger
-        </a-button>
-        <a-config-provider :auto-insert-space-in-button="false">
-            <a-button type="primary">
-                按钮
-            </a-button>
-        </a-config-provider>
-        <a-button type="primary">
-            按钮
-        </a-button>
-        <a-button type="link">
-            Link
-        </a-button>
+  <div id="acount">
+    <div class="form-wrap">
+      <a-form name="custom-validation" ref="ruleForm">
+        <a-form-item>
+          <label for="">用户名</label>
+          <a-input type="password" autocomplete="off" />
+        </a-form-item>
+        <a-form-item class="password">
+          <label for="">密码</label>
+          <a-input type="password" autocomplete="off" />
+        </a-form-item>
+        <!--                <a-form-item name="age">-->
+        <!--                    <div id="captcha-dom" class="nc-container"></div>-->
+        <!--                </a-form-item>-->
+        <a-form-item class="yzm">
+          <div id="captcha-dom" class="nc-container"></div>
+          <!--No-Captcha渲染的位置，其中 class 中必须包含 nc-container-->
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" html-type="submit" block>
+            登录
+          </a-button>
+        </a-form-item>
+      </a-form>
+      <div class="text-center fs-12">
+        <a href="" class="color-white">忘记密码</a> |
+        <a href="" class="color-white">注册</a>
+      </div>
     </div>
+  </div>
 </template>
 
-<script>
-    export default {
-        name: "login"
-    }
+<script lang="ts">
+import { reactive, onMounted } from "vue";
+// import "./captcha.js";
+export default {
+  name: "login",
+  setup(props) {
+    console.log(props);
+    const fromConfig = reactive({
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 }
+      }
+    });
+    onMounted(() => {});
+    return {
+      fromConfig
+    };
+  }
+};
 </script>
+<style lang="scss">
+#acount {
+  width: 100%;
+  /*height: 900px;*/
+  height: 100%;
+  background: #282828;
 
-<style scoped>
+  .form-wrap {
+    width: 285px;
+    color: #fff;
+    margin: 0 auto;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    /*border: 1px solid red;*/
 
+    label {
+      display: block;
+      color: #fff;
+      font-size: 14px;
+    }
+
+    .ant-col {
+      width: 100%;
+    }
+
+    .password {
+      margin-top: -15px;
+    }
+
+    .yzm {
+      margin-bottom: 25px;
+      margin-top: 20px;
+    }
+
+    .nc-container #nc_1_wrapper {
+      width: 100%;
+    }
+    .nc-container {
+      height: 34px;
+    }
+    .nc-container .nc_scale span {
+      height: 34px;
+    }
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,17 @@
 module.exports = {
     // 选项...
     publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
-    lintOnSave: process.env.NODE_ENV !== 'production'
-
+    lintOnSave: process.env.NODE_ENV !== 'production',
+    css: {
+        loaderOptions: {
+          // 给 sass-loader 传递选项
+          sass: {
+            // @/ 是 src/ 的别名
+            // 所以这里假设你有 `src/variables.sass` 这个文件
+            // 注意：在 sass-loader v8 中，这个选项名是 "prependData"
+            prependData: `@import "./src/style/main.scss";`
+          },
+        }
+      }
+ 
 }
